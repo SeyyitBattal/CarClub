d999c28fec3a642e57e0b2675c68bf8940633c3d
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2026-02-06 12:02:56 +0300

    Login/Register page added

--- login-service-frontend/src/app/app.ts
+++ login-service-frontend/src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('login-service-frontend');
+  mode : 'login' | 'register' = 'login';
+  setMode(newMode : 'login' | 'register'){
+    this.mode = newMode;
+  }
+
 }
